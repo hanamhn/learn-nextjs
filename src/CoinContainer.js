bf7b688f201cffb0c2f7ae08dd38aa4986078fb4
hanamhn <hanamhndev@gmail.com>
2020-11-16 09:47:48 +0700

    countinue

---
+++ src/CoinContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class CoinContainer extends Component {
+  state = {
+    currCoin: null,
+    nFlips: 0,
+    nHeads: 0,
+    nTails: 0
+  }
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default CoinContainer
